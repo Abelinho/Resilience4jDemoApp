f27118f34ec147815e2e972873d54c608f888b54
abel.agbachi <agbachiabel@gmail.com>
2023-04-21 20:18:01 +0100

    initial commit of created multi module project

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+è
+_democircuitbreaker/src/main/java/com/abel/democircuitbreaker/DemocircuitbreakerApplication.java,3\3\33f648dee50e3db4d7fb33ca591d4d1f69d1e21d
+J
+democircuitbreaker/pom.xml,1\d\1d2241cbcf8aec96edec6af6b22e214a211032c8
---
+++ demoretrypattern/src/main/java/com/abel/demoretrypattern/DemoretrypatternApplication.java
@@ -0,0 +1,13 @@
+package com.abel.democircuitbreaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocircuitbreakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemocircuitbreakerApplication.class, args);
+	}
+
+}
