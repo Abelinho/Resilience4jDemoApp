9edc685e6656437ff2147330a96516fcf009edd5
abel.agbachi <agbachiabel@gmail.com>
2023-04-23 11:31:41 +0100

    implemented customRetryHealthIndicator bean

--- demouserdetailsfacade/src/main/java/com/abel/demouserdetailsfacade/config/CustomRetryHealthIndicator.java
+++ demouserdetailsfacade/src/main/java/com/abel/demouserdetailsfacade/config/CustomRetryHealthIndicator.java
@@ -1,21 +1,23 @@
-//package com.abel.demouserdetailsfacade.config;
-//
-//import io.github.resilience4j.retry.RetryRegistry;
-//import org.springframework.boot.actuate.health.AbstractHealthIndicator;
-//import org.springframework.boot.actuate.health.Health;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class CustomRetryHealthIndicator extends AbstractHealthIndicator {
-//
-//    private final RetryRegistry retryRegistry;
-//
-//    public CustomRetryHealthIndicator(RetryRegistry retryRegistry) {
-//        this.retryRegistry = retryRegistry;
-//    }
-//
-//    @Override
-//    protected void doHealthCheck(Health.Builder builder) throws Exception {
-//        //you can programmatically configure retry pattern here
-//    }
-//}
+package com.abel.demouserdetailsfacade.config;
+
+import io.github.resilience4j.retry.RetryRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.AbstractHealthIndicator;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CustomRetryHealthIndicator extends AbstractHealthIndicator {
+
+    private final RetryRegistry retryRegistry;
+
+    @Autowired
+    public CustomRetryHealthIndicator(RetryRegistry retryRegistry) {
+        this.retryRegistry = retryRegistry;
+    }
+
+    @Override
+    protected void doHealthCheck(Health.Builder builder) throws Exception {
+        //you can programmatically configure retry pattern here
+    }
+}
